35203254ad248ca531b61b7643215ce8feb28ca3
Anton Zhiliakov <gofranich@gmail.com>
2022-02-06 14:11:21 +0600

    Add checkbox for test options

--- vue-frontend/src/components/IQuestionOne.vue
+++ vue-frontend/src/components/IQuestionOne.vue
@@ -1,18 +1,22 @@
 <template>
-    <div>
-        <h3>{{ iquestion.id }}. {{iquestion.name}}</h3>
-        <ul v-for="value in iquestion.testOptions" :key="value.id">
-            <li>{{value}}</li>
-        </ul>
+  <div>
+    <h3>{{ iquestion.id }}. {{ iquestion.name }}</h3>
+    <div v-for="(value, index) in iquestion.testOptions" :key="index">
+      <input type="checkbox" id="checkQuest" :value="value">
+      <label for="checkQuest">{{ value }}</label>
+      <br>
+      <br>
     </div>
+
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            iquestion: {},
-        }
-    }
+export default {
+  props: {
+    iquestion: {},
+  }
+}
 </script>
 
 <style scoped>
